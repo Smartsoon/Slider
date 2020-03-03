992cf846fd02d3f58164bde710720a82b8d09aed
Konstantin <aquile@smartapesmusic.com>
2020-03-03 22:56:09 +0200

    add posibility to add new images by link

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 class Slider {
-    constructor(imageArea, prevBtnId, nextBtnId) {
+    constructor(imageArea, prevBtnId, nextBtnId, addImages, addImagesBtn) {
         this.array = [];
         this.curImgIndex = 0;
         this.imgAreaId = document.querySelector(`${imageArea}`);
         this.prevBtn = document.querySelector(`${prevBtnId}`);
         this.nextBtn = document.querySelector(`${nextBtnId}`);
+        this.linkUploader = document.querySelector(`${addImages}`);
+        this.uploadButton = document.querySelector(`${addImagesBtn}`);
+        this.newImage = null;
     }
 
     turnPrev() {
@@ -25,20 +28,31 @@ class Slider {
         }
     }
 
+    addImagesByLink() {
+        this.newImage = this.linkUploader.value;
+        this.array.push(this.newImage);
+        this.linkUploader.value = ''
+    }
+
     add(arg) {
         this.array.push(arg);
         this.imgAreaId.src = this.array[this.curImgIndex];
         this.prevBtn.disabled = true
     }
 
+
     listenersReactions() {
         this.nextBtn.addEventListener('click', this.turnNext.bind(this));
-        this.prevBtn.addEventListener('click', this.turnPrev.bind(this))
+        this.prevBtn.addEventListener('click', this.turnPrev.bind(this));
+        this.uploadButton.addEventListener('click', this.addImagesByLink.bind(this))
     }
+
 }
 
-let slider = new Slider('.img', `.prev`, `.next`);
+let slider = new Slider('.img', `.prev`, `.next`, '.new-image', '.upload-button');
+
 slider.listenersReactions();
+
 slider.add('https://i.piccy.info/i9/4a1a61f5af233f8642db5f3bd32eff7a/1581343907/52101/1323468/car_one.jpg');
 slider.add('https://i.piccy.info/i9/fbeca19430af548dfb7a412425073a52/1581344152/133160/1323468/car_two.jpg');
 slider.add('https://i.piccy.info/i9/ed984155cd7aabf275693c7f2e72b7ef/1581344167/132961/1323468/car_three.jpg');
