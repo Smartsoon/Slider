acb5f4eaa21259f8753ddc47a3fc78d11f28d9d2
Konstantin <aquile@smartapesmusic.com>
2020-03-03 16:58:29 +0200

    simple slider

---
+++ js/script.js
@@ -0,0 +1,48 @@
+class Slider {
+    constructor(imageArea, prevBtnId, nextBtnId) {
+        this.array = [];
+        this.curImgIndex = 0;
+        this.imgAreaId = document.querySelector(`${imageArea}`);
+        this.prevBtn = document.querySelector(`${prevBtnId}`);
+        this.nextBtn = document.querySelector(`${nextBtnId}`);
+    }
+
+    turnPrev() {
+        this.curImgIndex--;
+        this.imgAreaId.src = this.array[this.curImgIndex];
+        this.nextBtn.disabled = false;
+        if (this.curImgIndex <= 0) {
+            this.prevBtn.disabled = true
+        }
+    }
+
+    turnNext() {
+        this.curImgIndex++;
+        this.imgAreaId.src = this.array[this.curImgIndex];
+        this.prevBtn.disabled = false;
+        if (this.curImgIndex >= this.array.length - 1) {
+            this.nextBtn.disabled = true
+        }
+    }
+
+    add(arg) {
+        this.array.push(arg);
+        this.imgAreaId.src = this.array[this.curImgIndex];
+        this.prevBtn.disabled = true
+    }
+
+    listenersReactions() {
+        this.nextBtn.addEventListener('click', this.turnNext.bind(this));
+        this.prevBtn.addEventListener('click', this.turnPrev.bind(this))
+    }
+}
+
+let slider = new Slider('.img', `.prev`, `.next`);
+slider.listenersReactions();
+slider.add('https://i.piccy.info/i9/4a1a61f5af233f8642db5f3bd32eff7a/1581343907/52101/1323468/car_one.jpg');
+slider.add('https://i.piccy.info/i9/fbeca19430af548dfb7a412425073a52/1581344152/133160/1323468/car_two.jpg');
+slider.add('https://i.piccy.info/i9/ed984155cd7aabf275693c7f2e72b7ef/1581344167/132961/1323468/car_three.jpg');
+slider.add('https://i.piccy.info/i9/b5ac09b67d445b6abcaf64edcd2fb2b7/1581344185/109148/1323468/car_four.jpg');
+
+
+
